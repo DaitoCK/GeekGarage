1a7d5959f5a4cadb5ae9470053722204d31782e8
kendhal <kendhalimax@gmail.com>
2021-02-08 11:31:30 +0100

    css detail

--- admin/db.php
+++ admin/db.php
@@ -2,7 +2,7 @@
 
 try{
     // Connexion à la base
-    $pdo = new PDO('mysql:host=mysql;dbname=geekgarage;host=127.0.0.1', 'root', '');
+    $pdo = new PDO('mysql:host=mysql;dbname=kendhalc_repare_online_garage;host=127.0.0.1', 'kendhalc', '5GCv159jrRHnuQ==');
     $pdo->exec('SET NAMES "UTF8"');
 } catch (PDOException $e){
     echo 'Erreur : '. $e->getMessage();
